f0981570601b60c75f30cb345715cca23063d22e
ITHackerstein <carelladavide1@gmail.com>
2025-03-08 10:11:33 +0100

    Span: Added function to merge spans.

--- src/Span.hpp
+++ src/Span.hpp
@@ -8,6 +8,14 @@ namespace bo {
 struct Span {
 	size_t start;
 	size_t end;
+
+	static Span merge(Span a, Span b) {
+		if (a.start > b.start) {
+			std::swap(a, b);
+		}
+
+		return Span { a.start, b.end };
+	}
 };
 
 auto format_as(Span) -> std::string;
